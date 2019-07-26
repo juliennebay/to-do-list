5a701856601be55e99d8b1d9433731eb35336d32
Julie <julienne.bay@gmail.com>
2019-07-26 15:54:56 -0400

    add keyup event listener

--- toDoList.js
+++ toDoList.js
@@ -1,15 +1,16 @@
 function loadScript(){
     const button = document.getElementsByTagName("button")[0]
+    const input = document.getElementById("task")
 
     function newMessage(event){
       const ul = document.getElementsByClassName("list")[0]
       const li = document.createElement("li")
-      const input = document.getElementById("task")
       ul.appendChild(li)
       li.textContent = input.value
       input.value = ""
     }
 
     button.addEventListener("click", newMessage)
+    input.addEventListener("keyup", e => e.keyCode === 13 && newMessage())
 }
 document.addEventListener("DOMContentLoaded", loadScript)
