e7cf3b5739efccedeae88d05dc8b4265d5bfcf6a
Julie <julienne.bay@gmail.com>
2019-08-20 16:32:10 -0400

    make sure checked off items stay checked off after refreshing page

--- toDoList.js
+++ toDoList.js
@@ -2,7 +2,7 @@ function loadScript(){
     const button = document.getElementsByTagName("button")[0]
     const taskInput = document.getElementById("task")
 //newMessage function creates the necessary HTML elements and binds event handlers to these elements.
-    function newMessage(value){
+    function newMessage(item){
         //line 5's parameter is either undefined (because line 63 calls it w/ nothing) 
         //or a string (from localStorage) (line 67 with forEach)
       const ul = document.getElementsByClassName("list")[0]
@@ -17,7 +17,7 @@ function loadScript(){
       checkboxItem.addEventListener("click", toggleStrikeThru)
 
       const newSpan = document.createElement("span")
-      const spanContent = document.createTextNode(value || taskInput.value)
+      const spanContent = document.createTextNode((item && item.todo) || taskInput.value)
       newSpan.appendChild(spanContent)
       newSpan.classList.add("p-1")
       li.appendChild(newSpan)
@@ -28,11 +28,19 @@ function loadScript(){
       deleteItem.classList.add("p-1", "cursor-pointer")
       deleteItem.addEventListener("click", deleteMessage)
 //the if statement below makes sure that items that already exist (in localstorage) aren't duplicated
-      if (!value) {
+      if (!item) {
         const todoItems = JSON.parse(localStorage.getItem("todos")) || []
         todoItems.push({todo: taskInput.value, checked: false})
         localStorage.setItem("todos", JSON.stringify(todoItems))
         taskInput.value = ""
+      }
+//this if statement makes sure that clicked items STAY clicked, even after refreshing page
+//we moved it down here, because toggleStrikeThru makes changes on span, 
+//which is the second child of li (the first child being the checkbox)
+//therefore, we must make sure that the span exists before clicking the checkbox item,
+//which is what calles toggleStrikeThru      
+      if (item && item.checked === true){
+        checkboxItem.click({target: { checked: true, parentElement: li}})
       }
     }
 //deleteMessage function hides the selected list item & also removes it from localStorage
@@ -64,7 +72,8 @@ function loadScript(){
     taskInput.addEventListener("keyup", e => e.keyCode === 13 && newMessage())
 //2. it calls newMessage with each item in localStorage
     const todoItems = JSON.parse(localStorage.getItem("todos"))
-    todoItems && todoItems.forEach(item => newMessage(item.todo))
+    todoItems && todoItems.forEach(item => newMessage(item))
+//by adding "todoItems &&", I ensure the forEach runs only when it's not undefined or null.
 }
 //3. it defines a bunch of functions (which, technically, it doesn't need to do)
 document.addEventListener("DOMContentLoaded", loadScript)
